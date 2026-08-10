965fb11f0d3b3d8b5fd75995203192d63b9d8bf3
sanjay-yadav-04 <im.sanjay.yadav.04@gmail.com>
2026-08-10 23:13:24 +0530

    m

---
+++ Q&A/advance/map,filter reduce/map.py
@@ -0,0 +1,102 @@
+# # Square each number
+# def square(x):
+#     return x*x
+# l=[1,2,3,4,5]
+# l1=list(map(square,l))
+# print(l1)
+
+# 2. Cube each number
+# def cube(x):
+#     return x*x*x
+# l=[1,2,3,4,5]
+# print(list(map(cube,l)))
+
+# 3. Convert strings to integers
+# def str(x):
+#     return int(x)
+# l=["1","2","3"]
+# print(list(map(str,l)))
+
+# 4. Convert to float
+# def flt(x):
+#     return float(x)
+# l=["2","4"]
+# print(list(map(flt,l)))
+
+# 5. Add 5 to each number
+# def add(x):
+#     return x+5
+# l=[1,2,3,4,5]
+# print(list(map(add,l)))
+
+# 6. Multiply by 2
+# def mul(x):
+#     return x*2
+# l=[1,2,3,4,5]
+# print(list(map(mul,l)))
+
+# 7. Convert to string
+# def val(x):
+#     return str(x)
+# l=[1,2,3]
+# print(list(map(val,l)))
+
+# 8. Absolute values
+# def val(x):
+#     return abs(x)
+# l=(-1,-5,3)
+# print(list(map(val,l)))
+
+# 9. Find length of strings
+# def le(x):
+#     return len(x)
+# l=["hi","python"]
+# print(list(map(le,l)))
+
+# 10. Convert to uppercase
+
+# name=["ram","shyam"]
+# print(list(map(str.upper,name)))
+
+# Check even or odd
+# def check(x):
+#     if x%2==0:
+#         return "Even"
+#     else:
+#         return "odd"
+# l=[1,2,3,4]
+# print(list(map(check,l)))
+
+
+# 22. Pass or Fail
+# def check(x):
+#     if x>40:
+#         return "pass"
+#     else:
+#         return "fAil"
+# l=[30,50,70]
+# print(list(map(check,l)))
+
+# 23. Square only even numbers
+# def sqonly(x):
+#     if(x%2==0):
+#         return x*x
+#     else:
+#         return x
+# l=[1,2,3,4,5]
+# print(list(map(sqonly,l)))
+
+# 24. Add bonus marks
+# def add(x):
+#     return x+5
+# l=[45,60]
+# print(list(map(add,l)))
+
+# 25. Check positive or negative
+# def check(x):
+#     if(x<0):
+#         return "negative"
+#     else:
+#         return "positive"
+# l=[-2,4,-1]
+# print(list(map(check,l)))
